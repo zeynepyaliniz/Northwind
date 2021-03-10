0144ea1d0e06d289b580781050e857b2f232e5e9
zeynep yalınız <zeynepyaliniz7@gmail.com>
2021-03-10 21:03:11 +0300

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Northwind';
+  title: string = 'Northwind';
+  user = 'Zeynep';
 }
