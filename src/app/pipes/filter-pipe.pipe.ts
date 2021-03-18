daa610ebc09a21f5941d385584e21ed63a1db056
zeynep yalınız <zeynepyaliniz7@gmail.com>
2021-03-18 23:34:23 +0300

    filterPipe parametric

--- src/app/pipes/filter-pipe.pipe.ts
+++ src/app/pipes/filter-pipe.pipe.ts
@@ -8,7 +8,8 @@ export class FilterPipePipe implements PipeTransform {
 
   transform(value:Product[], filterText:string): Product[] {
     filterText = filterText? filterText.toLocaleUpperCase():""
-    return filterText.length>2? value.filter(
+    let lenFilterText=2
+    return filterText.length>lenFilterText? value.filter(
       (p:Product)=>p.productName.toLocaleUpperCase().indexOf(filterText)!==-1):value;
   }
 
